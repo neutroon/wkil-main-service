7d52232be6b1ec7c9934bc1657d02abc0929d184
hesham <nbilha161@gmail.com>
2026-04-02 16:58:05 +0200

    refactor(crm.service): update LeadPayload interface to allow dynamic property keys for enhanced flexibility in lead data handling

--- src/services/crm/crm.service.ts
+++ src/services/crm/crm.service.ts
@@ -1,10 +1,7 @@
 import prisma from "../../config/prisma";
 
 export interface LeadPayload {
-  name: string;
-  email?: string;
-  phone?: string;
-  notes?: string;
+  [key: string]: any;
 }
 
 /**

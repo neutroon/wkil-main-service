c24dc103c693dafdd8253c5bed84d151f5eb1045
Hesham Mansour <113664536+neutroon@users.noreply.github.com>
2026-04-05 13:09:20 +0200

    Delete migrations directory

--- migrations/20251008190245_add_name_to_user/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Admin" ALTER COLUMN "name" DROP DEFAULT,
-ALTER COLUMN "updatedAt" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "public"."Lead" ALTER COLUMN "updatedAt" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "public"."User" ALTER COLUMN "name" DROP DEFAULT;
--- migrations/20260402021104_add_conversation_channel_phone/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Conversation" ADD COLUMN     "channel" TEXT,
-ADD COLUMN     "customerPhone" TEXT;
-
--- CreateIndex
-CREATE INDEX "Conversation_channel_idx" ON "public"."Conversation"("channel");

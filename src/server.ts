887d725c681ce23e28f18faaed00e426c4368abe
hesham <nbilha161@gmail.com>
2026-04-08 05:47:08 +0200

    feat(server): integrate HTTP server with socket.io for real-time communication, enhancing server capabilities

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
+import { createServer } from "http";
 import app from "./app";
 import dotenv from "dotenv";
+import { initSocket } from "./utils/socket";
 
 dotenv.config();
 
 const PORT = Number(process.env.PORT) || 8080;
 
-app.listen(PORT, "0.0.0.0", () => {
+const httpServer = createServer(app);
+initSocket(httpServer);
+
+httpServer.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });

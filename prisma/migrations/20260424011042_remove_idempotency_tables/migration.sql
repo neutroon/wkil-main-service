76383384f64b11dc05b548c280b27bb9bbc777df
hesham <nbilha161@gmail.com>
2026-04-24 04:11:40 +0300

    refactor(schema.prisma, migration.sql): remove idempotency models and associated migration to clean up unused database tables

---
+++ prisma/migrations/20260424011042_remove_idempotency_tables/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the `ProcessedFacebookComment` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `ProcessedMessengerMessage` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `ProcessedWhatsAppMessage` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "public"."ProcessedFacebookComment";
+
+-- DropTable
+DROP TABLE "public"."ProcessedMessengerMessage";
+
+-- DropTable
+DROP TABLE "public"."ProcessedWhatsAppMessage";

c93377af21bd5e116b74422205d987f439ca4e10
hesham <nbilha161@gmail.com>
2026-04-25 09:19:17 +0300

    refactor(lead.routes): integrate validation middleware for lead creation endpoint to ensure data integrity

--- src/routes/lead.routes.ts
+++ src/routes/lead.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { addLead, getLeads } from "../controllers/lead.controller";
+import { validate } from "../middlewares/validate.middleware";
+import { leadSchema } from "../validations/lead.validation";
 
 const leadRoutes = Router();
 
-leadRoutes.post("/", addLead);
+leadRoutes.post("/", validate(leadSchema), addLead);
 leadRoutes.get("/", getLeads);
 
 export default leadRoutes;

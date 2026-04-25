aeb9461fcd59677a8ec7d5e4326ed34c2a95f55e
hesham <nbilha161@gmail.com>
2026-04-25 13:07:41 +0300

    refactor(admin.service): remove unused authentication functions and imports to clean up admin service file

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -1,10 +1,6 @@
 import bcrypt from "bcrypt";
 import prisma from "../config/prisma";
 import { AppError } from "../middlewares/errorHandler.middleware";
-import {
-  generateAccessToken,
-  generateRefreshToken,
-} from "../middlewares/auth.middleware";
 
 const createAdmin = async (name: string, email: string, password: string) => {
   const hashed = await bcrypt.hash(password, 10);
@@ -35,49 +31,5 @@ const createAdmin = async (name: string, email: string, password: string) => {
     },
   });
 };
-
-// const login = async (email: string, password: string) => {
-//   const admin = await prisma.user.findUnique({
-//     where: { email },
-//     select: { id: true, name: true, email: true, password: true, role: true },
-//   });
-
-//   if (!admin) {
-//     throw new Error("Admin not found");
-//   }
-
-//   if (admin.role !== "admin") {
-//     throw new Error("Access denied - admin role required");
-//   }
-
-//   const valid = await bcrypt.compare(password, admin.password);
-//   if (!valid) {
-//     throw new Error("Invalid credentials");
-//   }
-
-//   // Generate tokens
-//   const accessToken = generateAccessToken({
-//     id: admin.id,
-//     name: admin.name,
-//     email: admin.email,
-//     role: admin.role,
-//   });
-
-//   const refreshToken = generateRefreshToken({
-//     id: admin.id,
-//     name: admin.name,
-//     email: admin.email,
-//     role: admin.role,
-//   });
-
-//   return {
-//     id: admin.id,
-//     name: admin.name,
-//     email: admin.email,
-//     role: admin.role,
-//     accessToken,
-//     refreshToken,
-//   };
-// };
 
 export { createAdmin };

58f1aad8ad6dc7ea8efac7db8585aa47621aa9cd
hesham <nbilha161@gmail.com>
2026-04-25 09:04:45 +0300

    refactor(lead.controller): simplify lead addition and retrieval by removing redundant try-catch blocks for cleaner code

--- src/controllers/lead.controller.ts
+++ src/controllers/lead.controller.ts
@@ -1,24 +1,15 @@
 import { Request, Response } from "express";
 import { createLead, getAllLeads } from "../services/lead.service";
-import prisma from "../config/prisma";
 
 const addLead = async (req: Request, res: Response) => {
-  try {
-    const { name, email, url, message } = req.body;
-    const lead = await createLead({ name, email, url, message });
-    res.status(201).json(lead);
-  } catch (err: any) {
-    res.status(400).json({ error: err.message });
-  }
+  const { name, email, url, message } = req.body;
+  const lead = await createLead({ name, email, url, message });
+  res.status(201).json(lead);
 };
 
 const getLeads = async (req: Request, res: Response) => {
-  try {
-    const leads = await getAllLeads();
-    res.status(200).json(leads);
-  } catch (err: any) {
-    res.status(400).json({ error: err.message });
-  }
+  const leads = await getAllLeads();
+  res.status(200).json(leads);
 };
 
 export { addLead, getLeads };

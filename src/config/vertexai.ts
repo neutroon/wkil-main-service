8d9c319b97a17cfdbd5ccc4361ebf846b85d7255
hesham <nbilha161@gmail.com>
2026-04-25 13:05:09 +0300

    refactor(vertexai.config): remove unused AppError import to streamline configuration file

--- src/config/vertexai.ts
+++ src/config/vertexai.ts
@@ -1,5 +1,5 @@
 import { VertexAI } from "@google-cloud/vertexai";
-import { AppError } from "../middlewares/errorHandler.middleware";
+
 import { env } from "./env";
 const projectId = env.GOOGLE_CLOUD_PROJECT_ID;
 const location = env.GOOGLE_CLOUD_LOCATION || "us-central1";

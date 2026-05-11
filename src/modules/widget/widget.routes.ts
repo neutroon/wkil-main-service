a3036b5e464fa6bd919d43783d4c8cf1f5a4c4f0
hesham <nbilha161@gmail.com>
2026-05-11 03:18:45 +0300

    refactor(widget.routes): remove crmIntegrations from businessProfile query to streamline data retrieval

--- src/modules/widget/widget.routes.ts
+++ src/modules/widget/widget.routes.ts
@@ -457,7 +457,6 @@ widgetRoutes.post(
         businessProfile: {
           include: {
             externalDataSources: { where: { isActive: true } },
-            crmIntegrations: { where: { isActive: true }, take: 1 },
           }
         } 
       },
